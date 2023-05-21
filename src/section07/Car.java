a6e0bd016ba51d63b0904421aa3df045bfc11671
ITSC <itsc@text.com>
2023-05-21 17:02:18 +0900

    생성자

--- src/section07/Car.java
+++ src/section07/Car.java
@@ -1,13 +1,25 @@
 package section07;
 
 public class Car {
-	int wheel=4;
-	//메소드 - 객체에서 기능역할
+	int wheel = 4;	// 바퀴 4개 
+					// 클래스 필드, 멤버필드 선언 및 초기화
+	
+	String color;
+	int camera;
+	
+	// 메소드 - 객체에서 기능역할
 	public static void drive() {
 		System.out.println("운전을 해요!");
 	}
+	
 	public void cleanUp() {
-		System.out.println("세차를 해요");
+		String str = "깨끗하게";
+		System.out.println("세차를 해요!");
 	}
-
+	
+	public void countWheel() {
+		System.out.println("바퀴 개수 : " + wheel);
+	}	
 }
+
+
